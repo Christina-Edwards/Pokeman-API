366d0fb01a1354e70694cb86bfed0f8e0802dbe3
Christina Edwards <cedwards239563@gmail.com>
2025-10-26 14:52:20 -0500

    added info to charizard.js

---
+++ charizrd.js
@@ -0,0 +1,9 @@
+const name = data.name;
+const weight = data.weight;
+
+const sprites = data.sprites;
+const frontImage = data.sprites.front_default;
+
+const abilitiesArray = data.abilities;
+const firstAbility = data.abilities[0];
+const firstAbilityName = data.abilities[0].ability.name;
